6811d6fdbf3d580fd01dc5f3fda963004e5f814d
aarush005 <asta005@gmail.com>
2025-01-17 13:14:28 +0530

    Modify Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion"
-
 import { styles } from "../styles"
 import { ComputersCanvas } from './canvas';
+import { SectionWrapper } from "../hoc";
 
 const Hero = () => {
   return (
@@ -24,21 +24,26 @@ const Hero = () => {
           </p>
         </div>
       </div>
-      <ComputersCanvas />
+      <ComputersCanvas 
+      width={window.innerWidth > 768 ? 800 : 400}
+      height={window.innerWidth > 768 ? 600 : 300} />
 
 <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
   <a href='#about'>
     <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
       <motion.div
-        animate={{
-          y: [0, 24, 0],
-        }}
-        transition={{
-          duration: 1.5,
-          repeat: Infinity,
-          repeatType: "loop",
-        }}
+        // animate={{
+        //   y: [0, 24, 0],
+        // }}
+        // transition={{
+        //   duration: 1.5,
+        //   repeat: Infinity,
+        //   repeatType: "loop",
+        // }}
         className='w-3 h-3 rounded-full bg-secondary mb-1'
+        style={{
+          display: window.innerWidth > 768 ? 'block' : 'none',
+        }}
       />
     </div>
   </a>
@@ -47,4 +52,4 @@ const Hero = () => {
 );
 };
 
-export default Hero
+export default (Hero, "hero")
